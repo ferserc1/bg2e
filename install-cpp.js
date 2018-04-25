561be69e86581a2ac8543ddcd3f4f4872d189819
Fernando Serrano Carpena <ferserc1@gmail.com>
2018-04-25 17:10:26 +0200

    Auto stash before merge of "master" and "origin/master"

--- install-cpp.js
+++ install-cpp.js
@@ -10,10 +10,8 @@ require('./scripts/install-deps.js').install()
         return Promise.all([
             git.clone('https://bitbucket.org/ferserc1/bg2engine.git','engine/bg2engine'),
             git.clone('https://bitbucket.org/ferserc1/bg2-model.git','engine/bg2-model'),
-            git.clone('https://bitbucket.org/ferserc1/fbx2json.git','engine/fbx2json'),
             git.clone('https://bitbucket.org/ferserc1/bg2e-viewer.git','engine/bg2e-viewer'),
-            git.clone('https://bitbucket.org/ferserc1/bg2e-vr.git','engine/bg2e-vr'),
-            git.clone('https://bitbucket.org/ferserc1/raytracer.git','engine/raytracer')
+            git.clone('https://bitbucket.org/ferserc1/bg2e-vr.git','engine/bg2e-vr')
         ]);
     })
 
