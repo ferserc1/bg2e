6461274d65dc6fea3e52d21e935bb18662d14904
Fernando Serrano Carpena <ferserc1@gmail.com>
2020-03-06 12:11:52 +0100

    Linux dependencies updated

--- scripts/dependencies.js
+++ scripts/dependencies.js
@@ -11,7 +11,7 @@ module.exports = {
             { url:`http://${ downloadServer }/${ dependencies }/lua-macOS-iOS-winVS2015-winVS2017-src.zip`, dest:"lua" },
             { url:`http://${ downloadServer }/${ dependencies }/openal-android-winVS2015-winVS2017.zip`, dest:"openal" },
             //{ url:`http://${ downloadServer }/${ dependencies }/openvr-master.zip`, dest:"openvr-master" },
-            { url:`http://${ downloadServer }/${ dependencies }/fbx2json-win64-osx.zip`, dest:"fbx2json-dist" },
+            { url:`http://${ downloadServer }/${ dependencies }/fbx2json-win64-osx-linux64.zip`, dest:"fbx2json-dist" },
             { url:`http://${ downloadServer }/${ dependencies }/bg2e-raytracer-win64-osx.zip`, dest:"bg2e-raytracer-dist" },
             { url:`http://${ downloadServer }/${ dependencies }/bg2e-scene-pkg-win64-osx.zip`, dest:"bg2e-scene-pkg" },
             { url:`http://${ downloadServer }/${ dependencies }/curl-win64-vs2017.zip`, dest:"curl" }
